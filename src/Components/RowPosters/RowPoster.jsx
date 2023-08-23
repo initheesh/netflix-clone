8a79796dbfa07b23a679f0f71a5b45035e225483
i_nitheesh <nitheeshks07@gmail.com>
2023-08-23 21:57:12 +0530

    Movie Trailer added

--- src/Components/RowPosters/RowPoster.jsx
+++ src/Components/RowPosters/RowPoster.jsx
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import './RowPoster.css'
 import axios from '../../axios'
-import {imageUrl} from '../../Constants/Constants'
+import {imageUrl,API_KEY} from '../../Constants/Constants'
+import YouTube from 'react-youtube'
 
 
 function RowPoster(props) {
   const [movie, setMovie] = useState([])
+  const [urlMovie, setUrlMovie] = useState('')
+  const opts = {
+      height: '390',
+      width: '100%',
+      
+    }
   useEffect(() => {
     axios.get(props.url).then((response)=>{
       console.log(response.data.results[0])
@@ -13,18 +20,35 @@ function RowPoster(props) {
     })
   }, [])
 
+  const handleMovieTrailer = (id)=>{
+    axios.get(`/movie/${id}/videos?api_key=${API_KEY}`).then(resp=>{
+      if(resp.data.results.length!==0){
+        setUrlMovie(resp.data.results[0])
+      }
+      else{
+        alert("No trialer available")
+        return
+      }
+        
+      
+    })
+  }
+
   
   return (
     <div className='row'>
         <h2 className="title">{props.title}</h2>
       <div className="posters">
       {movie.map((obj)=>{
-        return <img src={`${imageUrl+obj.backdrop_path}`} alt="movie poster" className='poster' />
+        
+        return <img src={`${imageUrl+obj.backdrop_path}`} alt="movie poster" className='poster' onClick={()=>handleMovieTrailer(obj.id)}/>
       })}
         
 
       </div>
+      {urlMovie &&  <YouTube videoId={urlMovie.key} opts={opts}  />}
     </div>
+    
   )
 }
 
